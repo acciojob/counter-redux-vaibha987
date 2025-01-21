810daf6e821231397d36889cf622c89d798caeb7
vaibha987 <v2goyal95@gmail.com>
2025-01-21 23:00:40 +0530

    Create CounterSlice.js

---
+++ src/components/CounterSlice.js
@@ -0,0 +1,20 @@
+import store from "./Store";
+import { createSlice } from '@reduxjs/toolkit';
+
+const counterSlice=createSlice({
+    name:"counter",
+    initialState:{
+     count:0   
+    },
+    reducers:{
+        increment:(state,action)=>{
+        state.count=state.count+1
+        },
+        decrement:(state,action)=>{
+         state.count=state.count-1
+        }
+    }
+})
+
+export const {increment,decrement} = counterSlice.actions
+export default counterSlice.reducer
