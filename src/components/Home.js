1f9cac49200c767fb214c72420bc551162e26293
vaibha987 <v2goyal95@gmail.com>
2025-01-21 23:01:09 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../Redux/CounterSlice'
+
+const Home = () => {
+  const dispatch=useDispatch()
+  const {count}=useSelector((state)=>state.counter)
+
+  const handleIncrement=()=>{
+    dispatch(increment())
+  }
+
+  const handleDecrement=()=>{
+   dispatch(decrement())
+  }
+  return (
+    <>
+    <p>{count}</p>
+    <button onClick={handleIncrement}>increment</button>
+    <button onClick={handleDecrement}>decrement</button>
+    </>
+  )
+}
+
+export default Home
